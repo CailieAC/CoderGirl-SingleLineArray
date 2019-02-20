a8bb850389c6aeee62937d0eca533222fe3f4006
Cailie Carlile <cailiecarlile@gmail.com>
2019-02-19 21:50:54 -0600

    commit1

--- SingleLineArray/Program.cs
+++ SingleLineArray/Program.cs
@@ -7,9 +7,13 @@ namespace SingleLineArray
         public static void Main()
         {
             // TODO: Initialize a single line an array with the following values: 1, 1, 2, 3, 5, 8. 
-
             // TODO: Loop through the array using foreach and print out each value on its own line.
+            int[] singleLineArray = new int[] { 1, 1, 2, 3, 5, 8 };
 
+            foreach (int arrayValue in singleLineArray)
+            {
+                Console.WriteLine(arrayValue);
+            }
             Console.ReadLine();
         }
     }
